a21e5b799db2ef988b325ff1b3c60f239481610d
Andy SHU <andy091045@gmail.com>
2020-05-01 21:40:34 +0800

    123

---
+++ Assets/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Score : MonoBehaviour
+{
+    public TextElement scoreText;
+    public float score;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    public void distroyCube()
+    {
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+}
