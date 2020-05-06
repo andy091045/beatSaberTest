61ec05aef752f4700258e8c0cb37b959d12ed6f2
Andy SHU <andy091045@gmail.com>
2020-05-06 20:27:24 +0800

    5/6

---
+++ Assets/SpawnerB.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerB : MonoBehaviour
+{
+    public GameObject[] cubes;
+    public Transform[] points;
+    public float beat = (60 / 130) * 2;
+
+    public sheepMusicB playMusic;
+    private float timer;
+
+    public int i = 0;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Debug.Log("B");
+    }
+    void Start()
+    {
+        Debug.Log("A");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(timer);
+        if (timer > beat && i < 4)
+        {
+
+            if (playMusic.music1[i] == 1)
+            {
+                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[0]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+            else if (playMusic.music1[i] == 2)
+            {
+                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[1]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+            else if (playMusic.music1[i] == 3)
+            {
+                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[2]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+            else if (playMusic.music1[i] == 4)
+            {
+                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[3]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+
+            timer -= beat;
+            i++;
+            Debug.Log("Spawner");
+        }
+        timer += Time.deltaTime;
+    }
+
+}
---
+++ Assets/sheepMusicR.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepMusicR : MonoBehaviour
+{
+    public int[] music1 = new int[] { 1, 2, 3, 4 };
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
