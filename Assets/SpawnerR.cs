492756bcc424e4db68482de578ee0b1b7946c2d8
Andy SHU <andy091045@gmail.com>
2020-05-07 16:16:26 +0800

    5555

--- Assets/SpawnerR.cs
+++ Assets/SpawnerR.cs
@@ -9,7 +9,7 @@ public class SpawnerR : MonoBehaviour
     public Transform[] points;
     public float beat = (60 / 130) * 2;
 
-    public sheepMusicR playMusic;
+    public sheepMusic playMusic;
     private float timer;
 
     public int i = 0;
@@ -33,24 +33,47 @@ public class SpawnerR : MonoBehaviour
         if (timer > beat && i < 4)
         {
 
-            if (playMusic.music1[i] == 1)
+            if (playMusic.musicRed[i] == 1)
             {
-                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[0]);
+                GameObject cube = Instantiate(cubes[0], points[0]);
                 cube.transform.localPosition = Vector3.zero;
             }
-            else if (playMusic.music1[i] == 2)
+            else if (playMusic.musicRed[i] == 2)
             {
-                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[1]);
+                GameObject cube = Instantiate(cubes[0], points[1]);
                 cube.transform.localPosition = Vector3.zero;
             }
-            else if (playMusic.music1[i] == 3)
+            else if (playMusic.musicRed[i] == 3)
             {
-                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[2]);
+                GameObject cube = Instantiate(cubes[0], points[2]);
                 cube.transform.localPosition = Vector3.zero;
             }
-            else if (playMusic.music1[i] == 4)
+            else if (playMusic.musicRed[i] == 4)
             {
-                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[3]);
+                GameObject cube = Instantiate(cubes[0], points[3]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+
+
+
+            if (playMusic.musicBlue[i] == 1)
+            {
+                GameObject cube = Instantiate(cubes[1], points[0]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+            else if (playMusic.musicBlue[i] == 2)
+            {
+                GameObject cube = Instantiate(cubes[1], points[1]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+            else if (playMusic.musicBlue[i] == 3)
+            {
+                GameObject cube = Instantiate(cubes[1], points[2]);
+                cube.transform.localPosition = Vector3.zero;
+            }
+            else if (playMusic.musicBlue[i] == 4)
+            {
+                GameObject cube = Instantiate(cubes[1], points[3]);
                 cube.transform.localPosition = Vector3.zero;
             }
 
---
+++ Assets/sheepMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepMusic : MonoBehaviour
+{
+
+    public int[] musicRed = new int[] { 1, 2, 3, 4 };
+    // 紅色方塊的譜面製作環境，依照1,2,3,4,順序代表由左至右的位置，0代表沒有東西
+    public int[] musicBlue = new int[] { 0, 1, 0, 1 };
+    // 藍色方塊的譜面製作環境，依照1,2,3,4,順序代表由左至右的位置，0代表沒有東西
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
